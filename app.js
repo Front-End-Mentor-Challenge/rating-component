c099ffc3949feda10458e150e58b17daf1187082
Anosha <anoshasohail676@gmail.com>
2023-07-27 15:48:07 +0500

    a

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const rating = document.querySelectorAll('.rating_circle');
   let value = 0;
   function handelRating(event) {
     value = parseInt(event.target.getAttribute('data-rating'));//event.target refers to the element which triggers the event
+   /*  for (let i = 0; i < rating.length; i++) {
+        rating[i].classList.remove("active")
+      } */
+      rating.forEach(circle => {
+        circle.classList.remove('active');
+      })
+      event.target.classList.add('active');
+
     
+ 
   }
   rating.forEach(circle => {
     circle.addEventListener('click', handelRating)
